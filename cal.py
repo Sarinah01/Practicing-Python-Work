e9ccd7df6ed22475ff81551f11ce18ac77361323
Sarinah <zaidchudiwala@gmail.com>
2025-12-15 23:55:32 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,13 @@
+def add(x, y): 
+     """Addition function."""
+     return x + y 
+def subtract(x, y):
+     """Subtraction function.""" 
+     return x- y 
+def multiply(x, y): 
+    """Multiplication function.""" 
+    return x * y 
+def divide(x, y): 
+     """Division function.""" 
+     if y != 0: return x / y 
+     else: return "Cannot divide by zero"
